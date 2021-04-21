31dcd9561330cb2e9b9b1f919ebe90ef992051bd
Tarik Agday <33735456+TarikAgday@users.noreply.github.com>
2021-04-21 10:44:40 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@
 
 import * as preproc from './scripts/preprocess.js'
 import * as viz from './scripts/viz.js'
-import * as heatmap from './scripts/heatmap'
-import * as connectedDotPlot from './scripts/connectedDotPlot'
-import * as bubbleChart from './scripts/bubbleChart'
+import * as heatmap from './scripts/heatmap.js'
+import * as connectedDotPlot from './scripts/connectedDotPlot.js'
+import * as bubbleChart from './scripts/bubbleChart.js'
 import * as scatterPlot from './scripts/scatterplot.js'
 import * as multiPannelPlot from './scripts/multiPannelBar.js'
 
