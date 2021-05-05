d9340420d052314b4982c4574701d6090b02d697
ines <ines.goddi@polymtl.ca>
2021-05-05 14:56:41 -0400

    ...

--- src/index.js
+++ src/index.js
@@ -29,6 +29,7 @@ $(function () {
     var dataHeatMap = preproc.heatmapProcess(files[0])
     heatmap.appendHeatMap(dataHeatMap)
 
+    d3.select("#viz_area_1").style("margin-left", "500")
     const scatteredPlotData = preproc.scatteredPlotProcess(files[1])
     scatterPlot.drawScatteredPlotChart(scatteredPlotData)
 
